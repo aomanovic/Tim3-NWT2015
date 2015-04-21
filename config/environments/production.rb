e72933db767c4b5efbf35cf1388e315f388770d8
aomanovic <aomanovic@outlook.com>
2015-04-21 13:04:48 +0200

    baza promjene config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,18 +78,18 @@ HealthMonitor::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # config.action_mailer.default_url_options = { :host => 'https://tim3-nwt.herokuapp.com/' }
-  # Rails.application.routes.default_url_options[:host] = 'https://tim3-nwt.herokuapp.com/'
-  #
-  # ActionMailer::Base.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #     address:              'smtp.gmail.com',
-  #     port:                 587,
-  #     domain:               'gmail.com',
-  #     user_name:            'healthmonitoretf',
-  #     password:             'nwt123456',
-  #     authentication:       'plain',
-  #     enable_starttls_auto: true  }
+  config.action_mailer.default_url_options = { :host => 'https://tim3-nwt.herokuapp.com/' }
+  Rails.application.routes.default_url_options[:host] = 'https://tim3-nwt.herokuapp.com/'
+
+  ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'healthmonitoretf',
+      password:             'nwt123456',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
 
   # reCAPTCHA keys
   RECAPTCHA_PUBLIC_KEY= '6LeV5wQTAAAAAA4uCs95tbEZwBNP55UlSCiI21lC'
