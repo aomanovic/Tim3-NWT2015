8798d2224178496bcdb7cb4071ff83d4c95e3afe
aomanovic <aomanovic@outlook.com>
2015-06-16 01:49:56 +0200

    heroku

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -2,15 +2,4 @@ Recaptcha.configure do |config|
   config.public_key  = ENV['RECAPTCHA_PUBLIC_KEY']
   config.private_key = ENV['RECAPTCHA_PRIVATE_KEY']
   config.api_version = 'v1'
-
-  #monkey patch to fix html_safe method
-  class String
-    def html_safe
-      self
-    end
-  end
-
-  # get an api key (free) from http://www.google.com/recaptcha
-  ENV['RECAPTCHA_PUBLIC_KEY'] = '6Lc-ZggTAAAAABT63pzCFfpKP397cQ0J5ecf6lPL'
-  ENV['RECAPTCHA_PRIVATE_KEY'] = '6Lc-ZggTAAAAAI9ERNUdEZUiZ8UVSOEfJvP17VB1'
 end
